a0cf80160828575b7982a5847dcb12ad13c55e70
sammypanda <38725335+sammypanda@users.noreply.github.com>
2022-05-06 16:06:03 +1000

    improved output folder management

--- extract-playlist.sh
+++ extract-playlist.sh
@@ -2,9 +2,12 @@
 
 working_dir=$(echo ${BASH_SOURCE[0]} | awk -F [a-z0-9\-]*[.]sh '{ print $1; print $3 }') # this doofus means we can't have more than one sh file in the working_dir
 
-if [[ -d $working_dir/output ]]; then
+if [ $(find $working_dir/output -maxdepth 1 -type f | wc -l) -gt 0 ]; then
+    # remove every file inside the output folder (ignores directories)
+    # TODO: tie this to a parameter
     rm $working_dir/output/*.*
 else
+    # create the output folder if it doesn't already exist
     mkdir $working_dir/output
 fi
 

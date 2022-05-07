8480502cafbe5ca4cea02ed25b43ab9b7dd70b2a
utsu-pinku <104600733+utsu-pinku@users.noreply.github.com>
2022-05-07 13:31:26 +0100

    Allowed user to establish path for output (#1)

--- extract-playlist.sh
+++ extract-playlist.sh
@@ -4,6 +4,7 @@ working_dir=$(echo ${BASH_SOURCE[0]} | awk -F [a-z0-9\-]*[.]sh '{ print $1; prin
 
 music_dir=/run/media/sammypanda/Storage/Music
 input=$1
+output_dir=$2
 
 if [ -n "$input" ]; then
     echo $input
@@ -20,10 +21,17 @@ for line in $(cat "$input"); do
     # echo $x
     if [[ "$line" =~ "$music_dir" ]]; then
         echo $line
-        cp "$line" "$working_dir/output"
+        cp "$line" "$output_dir"
     fi
 done
 unset IFS
 
 playlist=$(echo "$1" | cut -f  1 -d '.')
 notify-send "$USER" "Playlist '$playlist' Synced"
+
+## at the moment the program outputs to the output folder, add an option 
+
+path=$1
+
+
+## to let the user type the path (/path/to/folder) where the output files go, as opposed to being put into output folder
